4f245606e15aebd554500ff57abb200485809300
Dennis Lee <dennislwm@yahoo.com.au>
2023-08-31 12:12:09 +0800

    (feat) Add pytest

---
+++ app/tests/conftest.py
@@ -0,0 +1,17 @@
+import json
+import logging
+import pytest
+import yaml
+
+@pytest.fixture
+def config(logger):
+  with open('config.yml', mode='r') as f:
+    y=yaml.safe_load(f)
+    j=json.dumps(y)
+    logger.info(f"config.json: {j}")
+    f.close()
+  return j
+
+@pytest.fixture
+def logger():
+  return logging.getLogger(__name__)
